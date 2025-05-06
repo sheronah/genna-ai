48ad7f2dcbf35f69d935b216d9c6f252c6865e81
Francis Wetaka .S <franciswetaka@yahoo.com>
2025-05-06 18:54:24 +0300

    update - user authentication status, path fixes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import Provider from "./provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,14 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Provider> {children}</Provider>
-    
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Provider>{children}</Provider>
+
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
